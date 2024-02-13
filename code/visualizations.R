668a00ae832d34af761d55924047ea949bda2955
Aylin Seibold <ayla-99@live.de>
2024-02-13 20:01:43 +0100

    add boxplots

--- code/visualizations.R
+++ code/visualizations.R
@@ -1,10 +1,7 @@
 library(ggplot2)
-library(gridExtra)
+library(patchwork)
 library(dplyr)
-
-# Data Manipulation
-## This function connects the test data with the basic variables with the 
-## prediction for every model.
+library(cowplot)
 
 data.te <- X_U_TE
 data.fun <- function(model, data) {
@@ -37,10 +34,13 @@ gender.plot.fun <- function(data) {
     geom_boxplot() +
     theme_bw() + 
     coord_flip() +
-    labs(title = NULL,
-         x = "Predicted ZFYA",
-         y = "Gender")  +
-    guides(fill = "none")
+    guides(fill = "none") + 
+    xlim(-1.25,0.75) +
+    labs(title = NULL, ylab = "prediciton", xlab = "gender") +
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank()) +
+    scale_y_discrete(labels = c("male", "female"))
+    
 }
 
 # Boxplots Race
@@ -49,10 +49,12 @@ race.plot.fun <- function(data) {
     geom_boxplot() +
     theme_bw() + 
     coord_flip() +
-    labs(title = NULL,
-         x = "Predicted ZFYA",
-         y = "Race")  +
-    guides(fill = "none") 
+    xlim(-1.25,0.75) +
+    guides(fill = "none") + 
+  labs(title = NULL, ylab = "prediciton", xlab = "race") +
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank()) +
+    scale_y_discrete(labels = c("other", "mexican", "asian", "white", "black"))
 }
 
 
@@ -61,12 +63,26 @@ data.unaware <- data.fun("Unaware", data.te)
 data.K <- data.fun("FairK", data.te)
 data.Add <- data.fun("FairAdd", data.te)
 
-race.plot.fun(data.full)
-race.plot.fun(data.unaware)
-race.plot.fun(data.K)
-race.plot.fun(data.Add)
+plot1 <- gender.plot.fun(data.full) + labs(title = "Full") 
+plot2 <- gender.plot.fun(data.unaware) + labs(title = "Unaware") 
+plot3 <- gender.plot.fun(data.K) + labs(title = "Fair K")
+plot4 <- gender.plot.fun(data.Add) + labs(title = "Fair Add") 
 
-gender.plot.fun(data.full)
-gender.plot.fun(data.unaware)
-gender.plot.fun(data.K)
-gender.plot.fun(data.Add)
+plot5 <- race.plot.fun(data.full) + labs(title = "Full")
+plot6 <- race.plot.fun(data.unaware) + labs(title = "Unaware")
+plot7 <- race.plot.fun(data.K) + labs(title = "Fair K")
+plot8 <- race.plot.fun(data.Add) + labs(title = "Fair Add")
+
+combined.plot.gender <- plot1 + plot2 + plot3 + plot4 +
+  plot_layout(ncol = 2) 
+combined.plot.gender <- patchwork::patchworkGrob(combined.plot.gender)
+gridExtra::grid.arrange(combined.plot.gender, left = "Predictions", bottom = "Gender")
+
+combined.plot.race <- plot5 + plot6 + plot7 + plot8 + 
+  plot_layout(ncol = 2)
+combined.plot.race <- patchwork::patchworkGrob(combined.plot.race)
+gridExtra::grid.arrange(combined.plot.race, left = "Predictions", bottom = "Race")
+
+ggsave("Boxplot.Gender", plot = combined.plot.gender, device = "PNG", path = "../../plots", width = 160, height = 90, units = "mm")
+
+
